76bf947ab3891f6dc0118a05cf7c8cbfb32272f0
Yaron Grushka <yargru888@gmail.com>
2020-10-13 16:19:15 +0300

    connected to mongo

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -2,10 +2,6 @@ const express = require('express');
 const { getBootcamps, getBootcamp, createBootcamp, updateBootcamp, deleteBootcamp } = require('../controllers/bootcamps');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.status(200).json({ success: true, msg: 'Show all bootcamps' });
-});
-
 router.route('/').get(getBootcamps).post(createBootcamp);
 router.route('/:id').get(getBootcamp).put(updateBootcamp).delete(deleteBootcamp);
 
