a9a41942a4fc2297249b31552e94933490844848
Yaron Grushka <yargru888@gmail.com>
2020-10-15 21:47:54 +0300

    added courses

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,7 +1,15 @@
 const express = require('express');
-const { getBootcamps, getBootcamp, createBootcamp, updateBootcamp, deleteBootcamp } = require('../controllers/bootcamps');
+const { getBootcamps, getBootcamp, createBootcamp, updateBootcamp, deleteBootcamp, getBootcampsInRadius } = require('../controllers/bootcamps');
+
+// Include other resource routers
+const courseRouter = require('./courses');
+
 const router = express.Router();
 
+// Reroute into other resource routers
+router.use('/:bootcampId/courses', courseRouter);
+
+router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 router.route('/').get(getBootcamps).post(createBootcamp);
 router.route('/:id').get(getBootcamp).put(updateBootcamp).delete(deleteBootcamp);
 
