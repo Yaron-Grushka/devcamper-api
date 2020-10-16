c1c41f322f4be791736f140385761d0034f4aa96
Yaron Grushka <yargru888@gmail.com>
2020-10-16 19:52:49 +0300

    added advanced search middleware

--- seeder.js
+++ seeder.js
@@ -10,47 +10,49 @@ dotenv.config({ path: './config/config.env' });
 const Bootcamp = require('./models/Bootcamp');
 const Course = require('./models/Course');
 
-
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useFindAndModify: false,
+	useUnifiedTopology: true,
 });
 
 // Read JSON files
-const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8'));
+const bootcamps = JSON.parse(
+	fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
+);
 
-const courses = JSON.parse(fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8'));
-
+const courses = JSON.parse(
+	fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
+);
 
 // Import into DB
 const importData = async () => {
-    try {
-        await Bootcamp.create(bootcamps);
-        await Course.create(courses);
-        console.log('Data imported'.green.inverse);
-        process.exit();
-    } catch (error) {
-        console.error(error);
-    }
-}
+	try {
+		await Bootcamp.create(bootcamps);
+		await Course.create(courses);
+		console.log('Data imported'.green.inverse);
+		process.exit();
+	} catch (error) {
+		console.error(error);
+	}
+};
 
 // Delete data
 const deleteData = async () => {
-    try {
-        await Bootcamp.deleteMany();
-        await Course.deleteMany();
-        console.log('Data destroyed'.red.inverse);
-        process.exit();
-    } catch (error) {
-        console.error(error);
-    }
-}
+	try {
+		await Bootcamp.deleteMany();
+		await Course.deleteMany();
+		console.log('Data destroyed'.red.inverse);
+		process.exit();
+	} catch (error) {
+		console.error(error);
+	}
+};
 
-if(process.argv[2] === '-i') {
-    importData();
+if (process.argv[2] === '-i') {
+	importData();
 } else if (process.argv[2] === '-d') {
-    deleteData();
+	deleteData();
 }
